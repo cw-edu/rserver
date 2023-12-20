86f3288e4ac0986450660051b4bcb8b0346ee102
Cory <cory.a.weller@gmail.com>
2023-12-20 16:24:12 +0000

    move -y

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,5 +1,5 @@
-DEBIAN_FRONTEND=noninteractive apt-get update && \
-apt-get install -y libzmq3-dev 
+DEBIAN_FRONTEND=noninteractive apt-get -y update && \
+apt-get -y install libzmq3-dev 
 
 Rscript .devcontainer/install-r-packages.R
 
