d2f6204842f6e58e78f49ad58457ae675d327235
Cory <cory.a.weller@gmail.com>
2023-12-20 16:23:54 +0000

    add debian frontend

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,4 +1,6 @@
-apt-get update
-apt-get install libzmq3-dev 
+DEBIAN_FRONTEND=noninteractive apt-get update && \
+apt-get install -y libzmq3-dev 
 
 Rscript .devcontainer/install-r-packages.R
+
+unset DEBIAN_FRONTEND
