0ce764a27dd0adc0417baaa50f8a0935800f8d30
Cory <cory.a.weller@gmail.com>
2024-01-29 08:45:16 -0500

    Update setup.sh

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -3,6 +3,7 @@ apt-get -y update
 apt-get -y install libzmq3-dev jupyter
 
 R -e 'install.packages("data.table")'
+R -e 'install.packages("foreach")'
 R -e 'install.packages("ggthemes")'
 R -e 'install.packages("cowplot")'
 R -e 'install.packages("IRkernel")'
